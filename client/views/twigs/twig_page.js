f2b7b47d34c8c0c7a5d1b3cfdcf87cfd7c2d8788
haackthegenie <james@hackthegenie.com>
2013-07-17 15:29:37 +0100

    Updated to allow pressing the new button to create new blank Node linked back to parent

--- client/views/twigs/twig_page.js
+++ client/views/twigs/twig_page.js
@@ -4,3 +4,28 @@ Template.twigPage.helpers({
   },
 });
 
+Template.twigPage.events({
+		'click .new1': function(e) {
+		    e.preventDefault();
+		    var NewId = Twigs.insert({});
+		    alert(NewId);
+		    var currentTwigId = Session.get('currentTwigId');
+		    alert(currentTwigId);
+		    Twigs.update({'_id':currentTwigId}, {$set:{'twig1Id': NewId}});
+		    Meteor.Router.to('twigEdit', NewId);
+			},
+		'click .new2': function(e) {
+		    e.preventDefault();
+		    var NewId = Twigs.insert({});
+		    var currentTwigId = Session.get('currentTwigId');
+		    Twigs.update({'_id':currentTwigId},{$set:{'twig2Id': NewId}});
+		    Meteor.Router.to('twigEdit', NewId);
+			},
+		'click .new3': function(e) {
+		    e.preventDefault();
+		    var NewId = Twigs.insert({});
+		    var currentTwigId = Session.get('currentTwigId');
+		    Twigs.update({'_id':currentTwigId},{$set:{'twig3Id': NewId}});
+		    Meteor.Router.to('twigEdit', NewId);
+			}
+});
